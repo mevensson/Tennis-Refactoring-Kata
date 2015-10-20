9e77c88d22bd67ad7dbaa3366721e4b69530039e
Mattias Evensson <mattias@evensson.eu>
2015-10-20 18:26:22 +0200

    Extract getAdvantageOrWinScore method

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -24,16 +24,7 @@ public class TennisGame1 implements TennisGame {
 		if (isTied()) {
 			score = getTiedScore();
 		} else if (isAdvantageOrWin()) {
-			int minusResult = m_score1 - m_score2;
-			if (minusResult == 1) {
-				score = "Advantage player1";
-			} else if (minusResult == -1) {
-				score = "Advantage player2";
-			} else if (minusResult >= 2) {
-				score = "Win for player1";
-			} else {
-				score = "Win for player2";
-			}
+			score = getAdvantageOrWinScore();
 		} else {
 			for (int i = 1; i < 3; i++) {
 				if (i == 1) {
@@ -76,5 +67,20 @@ public class TennisGame1 implements TennisGame {
 
 	private boolean isAdvantageOrWin() {
 		return m_score1 >= 4 || m_score2 >= 4;
+	}
+
+	private String getAdvantageOrWinScore() {
+		String score;
+		int minusResult = m_score1 - m_score2;
+		if (minusResult == 1) {
+			score = "Advantage player1";
+		} else if (minusResult == -1) {
+			score = "Advantage player2";
+		} else if (minusResult >= 2) {
+			score = "Win for player1";
+		} else {
+			score = "Win for player2";
+		}
+		return score;
 	}
 }
