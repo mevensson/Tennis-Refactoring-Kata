f803a945ff791cf5674943c6760b929cbbc9e912
Mattias Evensson <mattias@evensson.eu>
2015-10-20 19:09:38 +0200

    Create scoreToString method and use where appropriate

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -16,60 +16,31 @@ public class TennisGame2 implements TennisGame {
 	public String getScore() {
 		String score = "";
 		if (P1point == P2point && P1point < 4) {
-			if (P1point == 0)
-				score = "Love";
-			if (P1point == 1)
-				score = "Fifteen";
-			if (P1point == 2)
-				score = "Thirty";
+			score = scoreToString(P1point);
 			score += "-All";
 		}
 		if (P1point == P2point && P1point >= 3)
 			score = "Deuce";
 
 		if (P1point > 0 && P2point == 0) {
-			if (P1point == 1)
-				P1res = "Fifteen";
-			if (P1point == 2)
-				P1res = "Thirty";
-			if (P1point == 3)
-				P1res = "Forty";
-
+			P1res = scoreToString(P1point);
 			P2res = "Love";
 			score = P1res + "-" + P2res;
 		}
 		if (P2point > 0 && P1point == 0) {
-			if (P2point == 1)
-				P2res = "Fifteen";
-			if (P2point == 2)
-				P2res = "Thirty";
-			if (P2point == 3)
-				P2res = "Forty";
-
+			P2res = scoreToString(P2point);
 			P1res = "Love";
 			score = P1res + "-" + P2res;
 		}
 
 		if (P1point > P2point && P1point < 4) {
-			if (P1point == 2)
-				P1res = "Thirty";
-			if (P1point == 3)
-				P1res = "Forty";
-			if (P2point == 1)
-				P2res = "Fifteen";
-			if (P2point == 2)
-				P2res = "Thirty";
+			P1res = scoreToString(P1point);
+			P2res = scoreToString(P2point);
 			score = P1res + "-" + P2res;
 		}
 		if (P2point > P1point && P2point < 4) {
-			if (P2point == 2)
-				P2res = "Thirty";
-			if (P2point == 3)
-				P2res = "Forty";
-			if (P1point == 1)
-				P1res = "Fifteen";
-			if (P1point == 2)
-				P1res = "Thirty";
+			P1res = scoreToString(P1point);
+			P2res = scoreToString(P2point);
 			score = P1res + "-" + P2res;
 		}
 
@@ -90,6 +61,17 @@ public class TennisGame2 implements TennisGame {
 		return score;
 	}
 
+	private String scoreToString(int score) {
+		assert(score <= 4);
+		if (score == 0)
+			return "Love";
+		if (score == 1)
+			return "Fifteen";
+		if (score == 2)
+			return "Thirty";
+		return "Forty";
+	}
+	
 	public void SetP1Score(int number) {
 
 		for (int i = 0; i < number; i++) {
