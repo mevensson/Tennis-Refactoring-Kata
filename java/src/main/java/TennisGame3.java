dec6d11d9f710e7aebd015a7e50da225743e8fcc
Mattias Evensson <mattias@evensson.eu>
2015-10-20 20:28:52 +0200

    Rename variables

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -11,16 +11,15 @@ public class TennisGame3 implements TennisGame {
 	}
 
 	public String getScore() {
-		String s;
 		if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-			String[] p = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
-			s = p[p1];
-			return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+			String[] scoreToString = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
+			String s = scoreToString[p1];
+			return (p1 == p2) ? s + "-All" : s + "-" + scoreToString[p2];
 		} else {
 			if (p1 == p2)
 				return "Deuce";
-			s = p1 > p2 ? p1N : p2N;
-			return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+			String leader = p1 > p2 ? p1N : p2N;
+			return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + leader : "Win for " + leader;
 		}
 	}
 
