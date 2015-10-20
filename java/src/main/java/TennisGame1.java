be5e090202ed3e4e9d0f0f0ca883b821de5884ca
Mattias Evensson <mattias@evensson.eu>
2015-10-20 18:34:19 +0200

    Remvove score variable in getAdvantageOrWinScore

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -45,18 +45,16 @@ public class TennisGame1 implements TennisGame {
 	}
 
 	private String getAdvantageOrWinScore() {
-		String score;
 		int minusResult = m_score1 - m_score2;
 		if (minusResult == 1) {
-			score = "Advantage player1";
+			return "Advantage player1";
 		} else if (minusResult == -1) {
-			score = "Advantage player2";
+			return "Advantage player2";
 		} else if (minusResult >= 2) {
-			score = "Win for player1";
+			return "Win for player1";
 		} else {
-			score = "Win for player2";
+			return "Win for player2";
 		}
-		return score;
 	}
 
 	private String getOtherScore() {
